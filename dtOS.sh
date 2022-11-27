5092b4aa6767199f27f5257348ee2391a325027c
Daniel Tasada <daniel.tasada@icloud.com>
2022-11-27 16:59:13 +0100

    edits to dtos.sh

--- dtOS.sh
+++ dtOS.sh
@@ -36,8 +36,11 @@ pacman -S zsh make vim exa ranger rclone alacritty firefox
 
 ### SETTING UP ZSH CONFIG ###
 
-mkdir -p ~/.config/shell mkdir ~/git/ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-mv .oh-my-zsh/ .config/shell
+mkdir -p ~/.config/shell/
+mkdir ~/git/
+cd ~/
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+mv ~/.oh-my-zsh/ ~/.config/shell
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
