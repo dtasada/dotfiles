1dce754caf9ab11a456dbff50ead5489a0012523
Daniel Tasada <daniel.tasada@icloud.com>
2022-11-27 16:28:11 +0100

    Commented in dtOS and added features

--- dtOS.sh
+++ dtOS.sh
@@ -1,10 +1,4 @@
-apt update && apt update
-apt install zsh make vim exa ranger rclone alacritty firefox nala
-pacman -Syyu
-pacman install zsh make vim exa ranger rclone alacritty firefox 
-dnf update
-dnf install zsh make vim exa ranger rclone alacritty firefox 
-
+### SETTING UP ZSH CONFIG ###
 mkdir -p ~/.config/shell
 mkdir ~/git/
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
@@ -14,6 +8,9 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/dylanaraps/pfetch ~/git/
+zsh
+
+### COPYING CONFIG FILES TO HOMEDIR
 
 cp .gitpush.sh ~/
 cp .p10k.zsh ~/
@@ -22,3 +19,17 @@ cp .zshrc ~/
 cp startup.sh ~/
 cp -r .vim ~/
 cp -r .config/* ~/.config/
+
+### INSTALLING PROGRAMS
+
+apt install nala
+syu
+s zsh make vim exa ranger rclone alacritty firefox 
+cd ~/git/pfetch && make install && cd ~/
+
+### INSTALLING BREW
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo '# Set PATH, MANPATH, etc., for Homebrew.' >> /home/dt/.zprofile
+echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/dt/.zprofile
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
