8409537d42a58c8018244c863856cb2f70f56358
Daniel Tasada <daniel.tasada@icloud.com>
2023-02-22 12:05:45 +0100

    Updated rofi

--- .config/waybar/scripts/powermenu.sh
+++ .config/waybar/scripts/powermenu.sh
@@ -5,31 +5,31 @@
 ## Github  : @adi1090x
 ## Twitter : @adi1090x
 
-dir="~/.config/waybar/scripts/rofi"
+dir="~/.config/rofi/themes/"
 uptime=$(uptime -p | sed -e 's/up //g')
 
-rofi_command="rofi -no-config -theme $dir/powermenu.rasi"
+rofi_command="rofi -no-config -theme ~/.config/rofi/themes/horizontal.rasi"
 
 # Options
 shutdown="⏻ Shutdown"
 reboot=" Restart"
 lock=" Lock"
-suspend="  Sleep"
+suspend=" Sleep"
 logout=" Logout"
 # Confirmation
 confirm_exit() {
 	rofi -dmenu\
-        -no-config\
+        #-no-config\
 		-i\
 		-no-fixed-num-lines\
 		-p "Are You Sure? : "\
-		-theme $dir/confirm.rasi
+		#-theme $dir/confirm.rasi
 }
 
 # Message
-msg() {
-	rofi -no-config -theme "$dir/message.rasi" -e "Available Options  -  yes / y / no / n"
-}
+#msg() {
+#	rofi -no-config -theme "$dir/message.rasi" -e "Available Options  -  yes / y / no / n"
+#}
 
 # Variable passed to rofi
 options="$lock\n$suspend\n$logout\n$reboot\n$shutdown"
@@ -37,53 +37,20 @@ options="$lock\n$suspend\n$logout\n$reboot\n$shutdown"
 chosen="$(echo -e "$options" | $rofi_command -p "Uptime: $uptime" -dmenu -selected-row 0)"
 case $chosen in
     $shutdown)
-		ans=$(confirm_exit &)
-		if [[ $ans == "yes" || $ans == "YES" || $ans == "y" || $ans == "Y" ]]; then
-			systemctl poweroff
-		elif [[ $ans == "no" || $ans == "NO" || $ans == "n" || $ans == "N" ]]; then
-			exit 0
-        else
-			msg
-        fi
+		systemctl poweroff
         ;;
     $reboot)
-		ans=$(confirm_exit &)
-		if [[ $ans == "yes" || $ans == "YES" || $ans == "y" || $ans == "Y" ]]; then
-			systemctl reboot
-		elif [[ $ans == "no" || $ans == "NO" || $ans == "n" || $ans == "N" ]]; then
-			exit 0
-        else
-			msg
-        fi
+		systemctl reboot
         ;;
     $lock)
-		# if [[ -f /usr/bin/i3lock ]]; then
-		# 	sh ~/.config/i3/scripts/blur-lock
-		# elif [[ -f /usr/bin/betterlockscreen ]]; then
-		# 	betterlockscreen -l
-		# fi
       exec swaylock
         ;;
     $suspend)
-		ans=$(confirm_exit &)
-		if [[ $ans == "yes" || $ans == "YES" || $ans == "y" || $ans == "Y" ]]; then
-			mpc -q pause
-			amixer set Master mute
-			systemctl suspend
-		elif [[ $ans == "no" || $ans == "NO" || $ans == "n" || $ans == "N" ]]; then
-			exit 0
-        else
-			msg
-        fi
+		mpc -q pause
+		amixer set Master mute
+		systemctl suspend
         ;;
     $logout)
-		ans=$(confirm_exit &)
-		if [[ $ans == "yes" || $ans == "YES" || $ans == "y" || $ans == "Y" ]]; then
-			kill Hyprland
-		elif [[ $ans == "no" || $ans == "NO" || $ans == "n" || $ans == "N" ]]; then
-			exit 0
-        else
-			msg
-        fi
+		killall Hyprland
         ;;
 esac
