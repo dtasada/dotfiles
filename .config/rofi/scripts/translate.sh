d6a9a5c3851f276ce107f1d1416850a6ea977fcf
Daniel Tasada <daniel.tasada@icloud.com>
2023-09-11 09:35:29 +0200

    update translate.sh

--- .config/rofi/scripts/translate.sh
+++ .config/rofi/scripts/translate.sh
@@ -4,4 +4,4 @@ str="$(rofi -dmenu -p 'translate str: ')"
 # brave https://translate.google.com/?sl=auto\&tl=$lang\&text=$(url_encode $word)\&op=translate
 translation="$(trans -b "$str" -target "$targetlang")"
 notify-send "Translate" "$translation"
-echo $translation | xclip  -selection c
+wl-copy $translation
