2ac04923f2a8a4e853ebeb1f64ac95722e890982
Daniel Tasada <daniel.tasada@icloud.com>
2023-10-10 12:30:33 +0200

    nvim

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -3,6 +3,7 @@ return {
 	"folke/neodev.nvim",
 	"michaelb/sniprun",
 	"lukas-reineke/indent-blankline.nvim", -- Indent line
+	{"lewis6991/gitsigns.nvim", config = function() require('gitsigns').setup() end },
 	{ "windwp/nvim-ts-autotag", config = function() require('nvim-ts-autotag').setup() end }, -- Close HTML tags
 	{ "windwp/nvim-autopairs", event = "InsertEnter", opts = {} } -- Close bracket and quote pairs
 }
