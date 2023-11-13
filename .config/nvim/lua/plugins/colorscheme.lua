81c695f075dc8f0e5b14db86a59115d38d53c189
Daniel Tasada <daniel.tasada@icloud.com>
2023-11-13 14:31:01 +0100

    gruvbox

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,6 +1,8 @@
 return {
-	"catppuccin/nvim", name = "catppuccin",
-	-- "Evalir/dosbox-vim-colorscheme", name = "dosbox",
+	-- "catppuccin/nvim", name = "catppuccin",
+
+	"Evalir/dosbox-vim-colorscheme", name = "dosbox",
+	-- "ellisonleao/gruvbox.nvim", name = "gruvbox",
 	-- "folke/tokyonight.nvim", name = "tokyonight",
 	-- "i3d/vim-jimbothemes", name = "jimbo",
 	-- "rebelot/kanagawa.nvim", name = "kanagawa",
@@ -13,12 +15,13 @@ return {
 
 		vim.cmd("colorscheme catppuccin-macchiato")
 
-		-- vim.cmd("colorscheme kanagawa-wave")
 		-- vim.cmd("colorscheme breakingbad")
 		-- vim.cmd("colorscheme dosbox")
+		-- vim.cmd("colorscheme gruvbox")
+		-- vim.cmd("colorscheme kanagawa-wave")
 		-- vim.cmd("colorscheme rose-pine-moon")
 		-- vim.cmd("colorscheme tokyonight")
-		-- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-		-- vim.api.nvim_set_hl(0, "Normal",      { bg = "none" })
+		vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+		vim.api.nvim_set_hl(0, "Normal",      { bg = "none" })
 	end
 }
