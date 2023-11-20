e0245621868be692b1e92b4ab55f13f0eaa0bce9
Daniel Tasada <daniel.tasada@icloud.com>
2023-11-20 18:55:09 +0100

    ultrawide

--- scripts/walls.sh
+++ scripts/walls.sh
@@ -2,19 +2,18 @@
 
 killall swaybg || killall feh
 
-DIR=~/walls/wides
-
 while true; do
-	rm -rf ~/walls/wides/
-	gert -m '3440' -l 1 -o ~/walls/ -H -s WidescreenWallpaper -f top -p all
-	mv ~/walls/WidescreenWallpaper ~/walls/wides
-
 	if [[ $(ps -e | grep -E "(swaybg|feh)" | wc -l) -gt 1 ]]; then
 		kill $(ps -e | grep -E "(swaybg|feh)" | head -1 | awk '{print $1}')
 	fi
 
+	gert -m '3440' -l 1 -o ~/walls -H -s WidescreenWallpaper -f top -p all
+	rm -rf ~/walls/wides/
+	mv ~/walls/WidescreenWallpaper ~/walls/wides
+	DIR=~/walls/wides
+
 	# feh --bg-fill --randomize ~/walls/catppuccin/ &
 	swaybg -i $DIR/$(\ls $DIR | shuf -n 1) &
 
-	sleep 2
+	sleep 90
 done
