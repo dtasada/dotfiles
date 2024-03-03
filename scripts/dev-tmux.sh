cdbf6d13cb89c6894e725b86935b14c9dc592951
Daniel Tasada <daniel.tasada@icloud.com>
2024-03-03 13:08:57 +0100

    asd

--- scripts/dev-tmux.sh
+++ scripts/dev-tmux.sh
@@ -2,19 +2,22 @@
 
 if [[ -z $1 ]]; then exit; fi
 
-# Create a new tmux session
 name=$(basename $1)
-tmux new-session -d -s $name -c $1 
+existing=$(tmux ls | awk -F : '{print $1}')
 
-# Split the first window horizontally and run the first command
-tmux send-keys -t $name:1 "exec nvim" C-m
+if [[ "$existing" == *"$name"* ]]; then
+	echo "Session already exists!"
+	tmux attach-session -t $name:1
+else
+	echo "Creating new session"
+	tmux new-session -d -s $name -c $1 
 
-# Split the second window vertically and run the second command
-tmux new-window -t $name:2 -n "lazygit" -c $1
-tmux send-keys -t $name:2 "exec lazygit" C-m
+	tmux send-keys -t $name:1 "exec nvim" C-m
 
-# Create a new window and run the third command
-tmux new-window -t $name:3 -n "shell" -c $1
+	tmux new-window -t $name:2 -n "lazygit" -c $1
+	tmux send-keys -t $name:2 "exec lazygit" C-m
 
-# Attach to the tmux session
-tmux attach -t $name:1
+	tmux new-window -t $name:3 -n "shell" -c $1
+
+	tmux attach -t $name:1
+fi
