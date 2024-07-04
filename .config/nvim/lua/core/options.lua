542b8cef4239939785c1d7f91d89731b427455ec
dtasada <83500532+dtasada@users.noreply.github.com>
2024-07-04 15:24:16 +0200

    Update options.lua

--- .config/nvim/lua/core/options.lua
+++ .config/nvim/lua/core/options.lua
@@ -29,3 +29,12 @@ vim.opt.updatetime = 50
 vim.opt.termguicolors = true
 
 vim.opt.mouse = 'a'
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+	group = vim.api.nvim_create_augroup("highlight_yank", {}),
+	desc = "Hightlight selection on yank",
+	pattern = "*",
+	callback = function()
+		vim.highlight.on_yank({ higroup = "IncSearch", timeout = 150 })
+	end,
+})
